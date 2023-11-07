78113746b3d137ef95d6fe09cd27f47c07526654
Tom Brzoza <tbbrzozaa@gmail.com>
2023-11-07 18:11:54 +0100

    Zadanie z czwartego dnia.

--- challengeApp/challengeApp/Program.cs
+++ challengeApp/challengeApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+string sex = "female";
+int age = 33;
+
+if (name == "Ewa" && sex == "female" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (sex == "female" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (sex == "male" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
