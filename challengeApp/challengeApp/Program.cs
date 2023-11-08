8062da956afcfd0d84e8e57912131da445f7e0a8
Tom Brzoza <tbbrzozaa@gmail.com>
2023-11-08 08:47:20 +0100

    Zadanie dzień 5.

--- challengeApp/challengeApp/Program.cs
+++ challengeApp/challengeApp/Program.cs
@@ -1,16 +1,69 @@
-﻿string name = "Ewa";
-string sex = "female";
-int age = 33;
+﻿var number = 1234567890;
+string numberAsString = number.ToString();
+char[] numbers = numberAsString.ToArray();
 
-if (name == "Ewa" && sex == "female" && age == 33)
+var counter0 = 0;
+var counter1 = 0;
+var counter2 = 0;
+var counter3 = 0;
+var counter4 = 0;
+var counter5 = 0;
+var counter6 = 0;
+var counter7 = 0;
+var counter8 = 0;
+var counter9 = 0;
+
+foreach (char digit in numbers)
 {
-    Console.WriteLine("Ewa, lat 33");
+    if (digit == '0')
+    {
+        counter0++;
+    }
+    else if (digit == '1')
+    {
+        counter1++;
+    }
+    else if (digit == '2')
+    {
+        counter2++;
+    }
+    else if (digit == '3')
+    {
+        counter3++;
+    }
+    else if (digit == '4')
+    {
+        counter4++;
+    }
+    else if (digit == '5')
+    {
+        counter5++;
+    }
+    else if (digit == '6')
+    {
+        counter6++;
+    }
+    else if (digit == '7')
+    {
+        counter7++;
+    }
+    else if (digit == '8')
+    {
+        counter8++;
+    }
+    else if (digit == '9')
+    {
+        counter9++;
+    }
 }
-else if (sex == "female" && age < 30)
-{
-    Console.WriteLine("Kobieta poniżej 30 lat");
-}
-else if (sex == "male" && age < 18)
-{
-    Console.WriteLine("Niepełnoletni Mężczyzna");
-}
+Console.WriteLine("Liczba " + number + " składa się z:");
+Console.WriteLine("0-" + counter0);
+Console.WriteLine("1-" + counter1);
+Console.WriteLine("2-" + counter2);
+Console.WriteLine("3-" + counter3);
+Console.WriteLine("4-" + counter4);
+Console.WriteLine("5-" + counter5);
+Console.WriteLine("6-" + counter6);
+Console.WriteLine("7-" + counter7);
+Console.WriteLine("8-" + counter8);
+Console.WriteLine("9-" + counter9);
