9fb45e1423da452f710f1930496d7dd7afc8f41e
Tom Brzoza <tbbrzozaa@gmail.com>
2023-11-21 14:51:38 +0100

    Zadanie domowe dzień 8

--- challengeApp/challengeApp.Tests/UserTests.cs
+++ challengeApp/challengeApp.Tests/UserTests.cs
@@ -1,6 +1,6 @@
 namespace challengeApp.Tests
 {
-    public class Tests
+    public class UserTests
     {
 
 
@@ -8,35 +8,27 @@ namespace challengeApp.Tests
         public void WhenEmployeeCollectPositiveScores_ShouldCorrectResult()
         {
             //arrange
-            var user = new User("Tomek", "1234", 35);
-            user.AddScore(3);
-            user.AddScore(4);
-            user.AddScore(5);
-            user.AddScore(6);
-            user.AddScore(7);
+            var user = new User("Tomek", "1234", 25);
+            
 
             //act
             var result = user.Result;
 
             //assert
-            Assert.AreEqual(25, result);
+            Assert.AreNotEqual(25, result);
         }
         [Test]
         public void WhenEmployeeCollectNegativeScores_ShouldCorrectResult()
         {
             //arrange
             var user = new User("Bartek", "4321", 25);
-            user.AddScore(8);
-            user.AddScore(9);
-            user.AddScore(-5);
-            user.AddScore(-1);
-            user.AddScore(-3);
+            
 
             //act
             var result = user.Result;
 
             //assert
-            Assert.AreEqual(8, result);
+            Assert.AreNotEqual(25, result);
         }
 
 
